fa26a096342e602f3526e376c5481e5688e3aaec
Emma Hopson <emmaehopson@icloud.com>
2023-12-05 16:56:22 -0700

    Updating the question.jsx

--- QuizQuest/src/Question.jsx
+++ QuizQuest/src/Question.jsx
@@ -1,41 +1,12 @@
 import {useState, useEffect} from 'react'
 
 
-export default function Question({ count, incrementCount }){
-    const url = "https://jsonplaceholder.typicode.com/users";
-  const [data, setData] = useState([]);
+export default function Question({question}){
 
-  const fetchInfo = () => {
-    return fetch(url)
-      .then((res) => res.json())
-      .then((d) => setData(d))
-  }
+    let writtenQuestion = question.question
 
-
-  useEffect(() => {
-    fetchInfo();
-  }, []);
 
   return (
-    <div className="App">
-      <h1 style={{ color: "green" }}>using JavaScript inbuilt FETCH API</h1>
-      <center>
-        {data.map((dataObj, index) => {
-          return (
-            <div
-              style={{
-                width: "15em",
-                backgroundColor: "#35D841",
-                padding: 2,
-                borderRadius: 10,
-                marginBlock: 10,
-              }}
-            >
-              <p style={{ fontSize: 20, color: 'white' }}>{dataObj.name}</p>
-            </div>
-          );
-        })}
-      </center>
-    </div>
+    <p></p>
   );
 }
